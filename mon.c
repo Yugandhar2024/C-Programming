297992e19d85fecaf6587998d3e37bea095cc24d
Yugandhar2024 <kunasaniyugandhar2004@gmail.com>
2024-10-30 15:21:22 +0530

    Create mon.c

---
+++ mon.c
@@ -0,0 +1,39 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+{ int a;
+printf("Enter the number between 1 and 12:");
+scanf("%d",&a);
+switch(a)
+{
+    case 1:printf("January");
+          break;
+    case 2:printf("Febuary");
+          break;
+    case 3:printf("March");
+          break;
+    case 4:printf("April");
+          break;
+    case 5:printf("May");
+          break;
+    case 6:printf("June");
+          break;
+    case 7:printf("July");
+          break;
+    case 8:printf("August");
+          break;
+    case9:printf("Septmber");
+          break;
+    case 10:printf("Octomber");
+           break;
+    case 11:printf("November");
+           break;
+    case 12:printf("December");
+           break;
+    default:printf("Invalid input");
+            break;
+}
+
+    return 0;
+}
