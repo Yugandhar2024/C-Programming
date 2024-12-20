50204b39e1da1042d57cdce3ccb68c854aa1d912
Yugandhar2024 <kunasaniyugandhar2004@gmail.com>
2024-12-20 11:50:56 +0530

    Create whil2.c

---
+++ whil2.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+    int i=0,j=10;
+    while(i<=10)
+    {
+        printf("%d ",i);
+        i++;
+    }
+    printf("\n");
+    while(j>=0)
+    {
+        printf("%d ",j);
+        j--;
+        
+    }
+    
+}
