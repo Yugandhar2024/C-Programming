6b10cc6e317924990077efe47c18d6b58567e1b6
Yugandhar2024 <kunasaniyugandhar2004@gmail.com>
2025-01-03 12:05:36 +0530

    Create pat5.c

---
+++ pat5.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i,j;
+    char n='A';
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c \t",n++);
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
