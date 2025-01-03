07a0485d7b2589e738dcf040b47f55239235daea
Yugandhar2024 <kunasaniyugandhar2004@gmail.com>
2025-01-03 12:00:04 +0530

    Create pat4.c

---
+++ pat4.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i,j,n=1;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d \t",n++);
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
